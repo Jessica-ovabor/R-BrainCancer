f0f4698e2af8d553897dd7babfd05e491d0d7928
Jessica-ovabor <ovaborjessica85@gmail.com>
2024-09-30 05:16:38 +0100

    extraction, normalisation and filtering

--- codes/biomarker_detection.R
+++ codes/biomarker_detection.R
@@ -61,3 +61,23 @@ simpleMeta <- data.frame("barcode" =gbm.data$barcode,
                          "race" = gbm.data$race,
                           "tumor_type" =gbm.data$tumor_descriptor)
 
+#Select datasets
+gbm.raw.data <- assays(gbm.data)
+
+#data size
+dim(gbm.raw.data$unstranded)
+#let's downsize our data to 5 primary and 5 recurring  data
+selectedBarcodes <- c(subset(simpleMeta, tumor_type == "Recurrence")$barcode[c(1:5)],
+                      subset(simpleMeta, tumor_type == "Primary")$barcode[c(1:5)])
+
+selectedData <- gbm.raw.data$unstranded[, c(selectedBarcodes)]
+dim(selectedData)
+           
+#Data normalization and filtering
+normData <- TCGAanalyze_Normalization(tabDF = selectedData, geneInfo = geneInfoHT, method = "geneLength")
+
+#Filtering
+filtData <- TCGAanalyze_Filtering(tabDF = normData,
+                                  method ="quantile",
+                                  qnt.cut= 0.25)
+
